584b732e684eb064e3619c85663f46cb9de8a105
Mammone <mammone@gmail.com>
2025-01-02 19:52:24 -0800

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Background from "@/components/background";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* <div className="bg-gradient-to-br from-cyan-600 to-sky-600"> */}
+          <Background/>
+          {children}
       </body>
     </html>
   );
---
+++ src/components/background.tsx
@@ -0,0 +1,16 @@
+
+export default function Background() {
+  return (
+    <div className="
+        glow absolute 
+        -z-10 
+        aspect-square 
+        w-full 
+        bg-gradient-to-r
+        from-cyan-600 
+        to-sky-600 
+        blur-3xl 
+        filter" 
+    />
+  )
+}
