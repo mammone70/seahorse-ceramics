b844928d16ce66a731e9a8ca208a57a48ee8d9aa
Mammone <mammone@gmail.com>
2025-02-16 08:45:38 -0800

    Implement getProductById and use it to populate product detail page

--- src/dao/products.tsx
+++ src/dao/products.tsx
@@ -1,9 +1,11 @@
 import { db } from "@/db"
 import { products } from "@/db/schemas/products"
+import { eq } from "drizzle-orm";
 
 export interface Product {
     id : number,
     name : string,
+    description : string,
     price : string | null,
     imageURL : string | null,
 }
@@ -11,4 +13,14 @@ export interface Product {
 export async function getProducts() : Promise<Product[]> {
     const prods = await db.select().from(products);
     return prods;
+}
+
+export async function getProductById(id : number) : Promise<Product>{
+    const prods = await 
+                        db
+                        .select()
+                        .from(products)
+                        .where(eq(products.id, id));
+    
+    return prods[0];
 }
