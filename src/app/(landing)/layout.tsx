51b8a46301feeac7d9e9009221a8deb7974a7634
Mammone <mammone@gmail.com>
2025-04-17 22:17:49 -0700

    Remove unused variables

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -45,7 +45,7 @@ import Footer from "@/components/footer"
 import type React from "react" // Added import for React
 import { CartSidebar } from "@/components/cart/cart-sidebar"
 import Providers from "@/providers"
-import ComingSoon from "./_sections/coming-soon"
+// import ComingSoon from "./_sections/coming-soon"
 
 export const metadata: Metadata = {
   title: "Handcrafted Ceramics",
@@ -61,14 +61,14 @@ export default async function RootLayout({
   modal: React.ReactNode
 }>) {
 
-  if(process.env.COMING_SOON){
-    return (
-      <>
-        <ComingSoon/>
-      </>
-    )
-  }
-  else
+  // if(process.env.COMING_SOON){
+  //   return (
+  //     <>
+  //       <ComingSoon/>
+  //     </>
+  //   )
+  // }
+  // else
     return (
         <>
           {/* <Background/> */}
